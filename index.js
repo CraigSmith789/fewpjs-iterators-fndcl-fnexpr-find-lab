812f779b7f633133438aa6312e6b5b47e5a0fb7a
CraigSmith789 <craig789@gmail.com>
2021-02-08 13:18:27 -0500

    commit tree 3244ccd1c3b0137f92c3ffbfad295790bd6e64e1

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}]
+
+function superbowlWin(array){
+  
+ let a = array.find(array => array.result === "W") 
+//  debugger  
+return !!a ? a.year : undefined
+  
+};
+
+
+
+superbowlWin(record)
+
+
+
+
